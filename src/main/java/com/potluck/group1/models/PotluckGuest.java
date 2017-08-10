8fea039f0754d5a159c7d6dc08ad15bd7a6ba83b
nmerris <nate.merris@gmail.com>
2017-08-10 09:47:01 -0400

    First commit- Model created by Group 1

---
+++ src/main/java/com/potluck/group1/models/PotluckGuest.java
@@ -0,0 +1,37 @@
+package com.potluck.group1.models;
+
+
+public class PotluckGuest {
+
+    private String firstName;
+    private String lastName;
+    private String dishTitle;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDishTitle() {
+        return dishTitle;
+    }
+
+    public void setDishTitle(String dishTitle) {
+        this.dishTitle = dishTitle;
+    }
+
+
+
+
+}
