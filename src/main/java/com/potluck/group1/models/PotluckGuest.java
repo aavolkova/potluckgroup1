af2602e85e8e2a9a01e5b1d7b89ad75c0eba37e9
nmerris <nate.merris@gmail.com>
2017-08-10 13:59:15 -0400

    added validation and error messages

--- src/main/java/com/potluck/group1/models/PotluckGuest.java
+++ src/main/java/com/potluck/group1/models/PotluckGuest.java
@@ -5,18 +5,31 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 public class PotluckGuest {
 
+    @NotNull
+    @Size(min=3,max=30)
     private String firstName;
     private String lastName;
+    @NotNull
+    @Size(min=3,max=100)
     private String dishTitle;
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 
     public String getFirstName() {
         return firstName;
